37a57a971db2b0eeab8aa8e85a589125460d7fad
ElijDavis <elijahadavis@hotmail.com>
2025-07-03 19:41:43 -0400

    Changed the cors config, lets see if it works

--- src/main/java/com/example/assignment4/config/CorsConfig.java
+++ src/main/java/com/example/assignment4/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.example.assignment4.config;
+/*package com.example.assignment4.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
@@ -9,6 +9,24 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:3000", "https://portfolio-zeta-lemon-73.vercel.app/Algorithms", "https://algorithms-api.onrender.com") // TS dev server
+                .allowedMethods("*");
+    }
+}*/
+
+package com.example.assignment4.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                    "http://localhost:3000", 
+                    "https://portfolio-zeta-lemon-73.vercel.app"
+                )
                 .allowedMethods("*");
     }
 }
